2936a23776dd75203047c8adcdcff4610317028b
fxxwol <protsivroksolana2004@gmail.com>
2023-04-08 18:47:13 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients")
+
+const makeLiEl = name => { 
+  const liEl = document.createElement("li")
+  liEl.textContent = name
+  liEl.classList.add("item")
+  return liEl
+}
+
+const allLiEl = ingredients.map(makeLiEl);
+ingredientsEl.append(...allLiEl)
