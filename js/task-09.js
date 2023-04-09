062da68cc1574cff1c9797d476189fc58b15a0ad
fxxwol <protsivroksolana2004@gmail.com>
2023-04-09 17:44:18 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.body,
+  span: document.querySelector('.color'),
+  btn: document.querySelector('.change-color')
+}
+
+refs.btn.addEventListener('click', onBtnClick)
+
+function onBtnClick() { 
+  const bcgColor = getRandomHexColor();
+  refs.body.style.backgroundColor = `${bcgColor}`
+  refs.span.textContent = `${bcgColor}`
+}
