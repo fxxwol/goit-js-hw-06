cf431444fe24779a0cc95b4ed9902d20c318103f
fxxwol <protsivroksolana2004@gmail.com>
2023-04-09 03:09:08 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    const currentForm = event.currentTarget;
+    console.log(currentForm.elements)
+    const formData = new FormData(currentForm)
+    const user = {}
+
+    formData.forEach((value, name) => {
+        if (value === '') {
+            alert("Fill in all fields")
+        }
+        else {
+            user[name] = value
+        }
+    })
+    console.log(user)
+    currentForm.reset()
+}
