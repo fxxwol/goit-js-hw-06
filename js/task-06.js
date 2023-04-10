994978e467109deda2080c2885cb4c8cc314bdc2
fxxwol <protsivroksolana2004@gmail.com>
2023-04-11 00:45:31 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,17 @@ const input = document.querySelector('#validation-input')
 input.addEventListener('blur', onInputBlur)
 
 function onInputBlur(event) {
-    event.currentTarget.value.length === 6 ?
-        event.currentTarget.classList.add('valid') :
+    console.log(event.currentTarget.value.length)
+    if (event.currentTarget.value.length === 6) {
+        event.currentTarget.classList.toggle('valid')
+        if (event.currentTarget.classList.contains('invalid')) {
+            event.currentTarget.classList.remove('invalid')
+        }
+    }
+    else { 
         event.currentTarget.classList.add('invalid')
+        if (event.currentTarget.classList.contains('valid')) {
+            event.currentTarget.classList.remove('valid')
+        }
+    }
 }
