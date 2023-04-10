dd076c456870646dad0b3964db5548a3d0638e6d
fxxwol <protsivroksolana2004@gmail.com>
2023-04-10 14:37:38 +0300

    task 6 and 7

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#validation-input')
 input.addEventListener('blur', onInputBlur)
 
 function onInputBlur(event) {
-    event.currentTarget.value.length <= 6 ?
+    event.currentTarget.value.length === 6 ?
         event.currentTarget.classList.add('valid') :
         event.currentTarget.classList.add('invalid')
 }
--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,7 @@ const refs = {
     input: document.querySelector('#font-size-control'),
     span: document.querySelector('#text')
 }
+refs.span.style.cssText = `font-size: ${refs.input.value}px`
 
 refs.input.addEventListener('input', onInput)
 
